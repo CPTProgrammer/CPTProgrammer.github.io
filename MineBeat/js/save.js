b08bf99f07b81fbc5b00b7a6e34348374d0629de
CPTProgrammer <1807310025@qq.com>
2020-11-03 22:18:34 +0800

    MineBeat Update 2020/11/03
    MineBeat Update 2020/11/03

--- MineBeat/js/save.js
+++ MineBeat/js/save.js
@@ -3,14 +3,25 @@
 举个例子
 JSON:
 {
-    dragPercent: ,
+    settings: {
+        dragPercent: ,
+        openedPages: []
+    },
     file: [
         {
             fileName: "", //<-文件名称
-            fileSuffix: "", //<-文件后缀 "{folder}"为文件夹
+            fileSuffix: "", //<-文件后缀 "{folder}"为文件夹 "mb"为MineBeat音乐保存文件
             fileContent: "" //<-文件内容 当fileSuffix为"{folder}"时存储[]数组（套娃）
         }
     ],
+}
+
+
+"*.mb"
+{
+    date: , //<-时间（毫秒）
+    author: "", //<-作者
+    ......
 }
 
 */
